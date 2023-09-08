9145c2f7042da49f186dc22169e90304f3be76d8
Veaveslav Chirita <you@example.com>
2023-09-08 16:22:34 +0200

    refreshing access tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://api-destinations-941abf2d2aea.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
